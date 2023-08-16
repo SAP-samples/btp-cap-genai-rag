d0f65269e09aa2d0002fd7a493795d60aee82249
Martin Frick <martin.frick@sap.com>
2023-08-16 07:28:41 +0200

    Push latest refactoring state

--- code/broker/start.js
+++ code/broker/start.js
@@ -2,14 +2,10 @@ import Broker from '@sap/sbf';
 
 // If VCAP_APPLICATION is defined, we are in a Cloud Foundry scenario
 // For local testing, the USER and PASSWORD are always injected via environment variables
-if(process.env.VCAP_APPLICATION && process.env["NODE_ENV"] === "production"){ 
+if(process.env.VCAP_APPLICATION && process.env.NODE_ENV === "production"){ 
     new Broker().start();
-
 }else{
     // In production Kyma scenarios and local testing, BROKER_USER and BROKER_PASSWORD are passed in env variables
-    let brokerConfig = { brokerCredentials : { [process.env["BROKER_USER"]] : process.env["BROKER_PASSWORD"] }}
-    // For local testing scenario, if SBF_CATALOG is part of environment variables, include it in broker config
-    process.env["SBF_CATALOG"] && ( brokerConfig = { ...brokerConfig, catalog : JSON.parse(process.env["SBF_CATALOG"]) } )
-    
+    let brokerConfig = { brokerCredentials : { [process.env["BROKER_USER"]] : process.env["BROKER_PASSWORD"] }}    
     new Broker(brokerConfig).start()
 }
